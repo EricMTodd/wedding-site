bcb3a47b5ec62872a1a85b7939527bdfde3258af
EricMTodd <eterictodd@gmail.com>
2024-01-17 18:35:57 -0700

    Pls god push

---
+++ README.md
@@ -0,0 +1,9 @@
+# wedding-site
+youtube video for python environments: https://www.youtube.com/watch?v=N5vscPTWKOk
+create: virtualenv (project-name)
+activate: source (project-name)/bin/activate
+deactivate: deactivate
+create requirements file: pip freeze --local > requirements.txt
+isntall requirements from file: pip install -r requirements.txt
+check requirements file: cat requirements.txt
+see installed pips: pip list
---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template
+from datetime import date
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+	today = date.today()
+	event = date(2024, 9, 16)
+	delta = event - today
+	return render_template('index.html', delta=delta)
+
+@app.route('/event-details')
+def event_details():
+	return render_template('event-details.html')
+
+@app.route('/gallery')
+def gallery():
+	return render_template('gallery.html')
+
+@app.route('/rsvp')
+def rsvp():
+	return render_template('rsvp.html')
+
+@app.route('/travel-and-stay')
+def travel_and_stay():
+	return render_template('travel-and-stay.html')
