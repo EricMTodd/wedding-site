d94a2dc5b48efdbe00148c8c80271fd682fb2318
Eric M. Todd <eterictodd@gmail.com>
2024-03-19 20:58:47 -0600

    Added production server

--- app.py
+++ app.py
@@ -53,4 +53,4 @@ def travel_and_stay():
 	return render_template('travel-and-stay.html')
 
 if __name__== '__main__':
-      app.run(host='0.0.0.0', port=8000)
+      app.run(debug=True, host='0.0.0.0', port=8000)
